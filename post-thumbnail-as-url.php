d2e5335c4a3aa6ab7351e43a463a6ed0e1001990
niwatolli3 <niwatolli3@gmail.com>
2018-04-05 22:12:30 +0900

    refs #1 * add template plugin

---
+++ post-thumbnail-as-url.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+Plugin Name: Leach! Post Thumbnail As URL
+Plugin URI: https://blog.leach.tokyo/2018/04/04/leachpost-thumbnail-as-url/
+Description: Post Thumbnail As not image, but URL
+Version: 1.0.0
+Author: niwatolli3 <niwatolli3@gmail.com>
+Author URI: https://blog.leach.tokyo/
+License: GPL2
+*/
+
+/*  Copyright 2018 niwatolli3  (email : niwatolli3@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
